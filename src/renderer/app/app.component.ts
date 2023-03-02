376cecf3583c28a0ef682901526fa0132f108588
haht <haht@creativeforce.io>
2023-03-02 23:56:03 +0700

    configure tailwindCss

--- src/renderer/app/app.component.ts
+++ src/renderer/app/app.component.ts
@@ -1,22 +1,17 @@
-import { HeroService } from '@services/hero.service';
-import {AfterViewInit, Component} from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit{
-  title = 'electronjs-app';
+export class AppComponent implements AfterViewInit {
+  title = 'Kelvin Next';
 
-  constructor(
-    public hero: HeroService
-  ) {
-  }
+  constructor() {}
 
   ngAfterViewInit() {
     console.log(environment.api);
-
   }
 }
