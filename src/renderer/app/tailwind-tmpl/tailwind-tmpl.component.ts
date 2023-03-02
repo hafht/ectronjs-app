c0c71ee1d8042204221df50d72996284035519c5
haht <haht@creativeforce.io>
2023-03-03 01:09:34 +0700

    tailwind css quick start

---
+++ src/renderer/app/tailwind-tmpl/tailwind-tmpl.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tailwind-tmpl',
+  templateUrl: './tailwind-tmpl.component.html',
+  styleUrls: ['./tailwind-tmpl.component.scss']
+})
+export class TailwindTmplComponent {
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./src/renderer/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '576px',
+        // => @media (min-width: 576px) { ... }
+
+        md: '960px',
+        // => @media (min-width: 960px) { ... }
+
+        lg: '1440px',
+        // => @media (min-width: 1440px) { ... }
+      },
+      spacing: {
+        35: '35px',
+      },
+      colors: {
+        primary: '#3E82FB',
+      },
+    },
   },
   plugins: [],
 };
